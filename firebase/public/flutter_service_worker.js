6ca6dc312f2512c62a6fe8819f9b03e819b4df01
pinkfish <pinkfishfrog@gmail.com>
2020-06-09 18:39:03 +0000

    Publish the firebase hosting from GitHub action

--- firebase/public/flutter_service_worker.js
+++ firebase/public/flutter_service_worker.js
@@ -1,26 +1,26 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "0d04e4404a71b6c3f318ed43d4942dca",
-"/": "0d04e4404a71b6c3f318ed43d4942dca",
-"main.dart.js_1.part.js.map": "5daa5995072dd49f7bcfeddfbcafe1f5",
-"favicon-16x16.png": "5b2d89811dbdde45780850863d4d913e",
-"icons/android-chrome-192x192.png": "105071af0f043eddd6359069f265b005",
-"icons/apple-touch-icon.png": "9e9199ec9d9b60282c33224926f1ade4",
-"icons/android-chrome-512x512.png": "6afade3662b1e346b6d46594f8761b8e",
-"icons/favicon-32x32.png": "16e360dc930c64da4abe854882318342",
-"favicon.ico": "e90e558b28216bbfb20b734fe6b05fea",
+  "favicon.ico": "e90e558b28216bbfb20b734fe6b05fea",
+"main.dart.js": "8f6dd2d7506004ea7e527150543bb5ef",
+"main.dart.js_1.part.js": "3f07f10a00050de7db45838a0a951f20",
+"main.dart.js_1.part.js.map": "72cb716d15f2c476b9a66473c37048be",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/FontManifest.json": "0621fb7723859a382fc19210904f6578",
+"assets/LICENSE": "145a7156022925f73584dc242354a1ad",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "fe1545ef4dd1eef2f1e25528898fc0b3",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/assets/images/google_logo.png": "b75aecaf9e70a9b1760497e33bcd6db1",
 "assets/assets/images/basketball.png": "8055b697296385573ac53b1a12922da0",
-"assets/FontManifest.json": "0621fb7723859a382fc19210904f6578",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/AssetManifest.json": "17c2fcb0f84c471fe2c927bed2c859d8",
-"assets/LICENSE": "49e106ceae6b4906fad93a2610f3727e",
-"main.dart.js_1.part.js": "dbaa6f1692098793c68a1d1d55b0dd24",
-"main.dart.js": "ddf22ea03543af5f21e20b5bf560e1b5",
-"manifest.json": "f4622512655cfd8837f1e6ef2a4ebbfa"
+"favicon-16x16.png": "5b2d89811dbdde45780850863d4d913e",
+"manifest.json": "f4622512655cfd8837f1e6ef2a4ebbfa",
+"icons/apple-touch-icon.png": "9e9199ec9d9b60282c33224926f1ade4",
+"icons/android-chrome-512x512.png": "6afade3662b1e346b6d46594f8761b8e",
+"icons/android-chrome-192x192.png": "105071af0f043eddd6359069f265b005",
+"icons/favicon-32x32.png": "16e360dc930c64da4abe854882318342",
+"index.html": "0d04e4404a71b6c3f318ed43d4942dca",
+"/": "0d04e4404a71b6c3f318ed43d4942dca"
 };
 
 self.addEventListener('activate', function (event) {
--- firebase/public/main.dart.js_1.part.js
+++ firebase/public/main.dart.js_1.part.js
@@ -1,10 +1,10 @@
 self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
 $__dart_deferred_initializers__.current=function(a,b,c,$){var C,H,J,P,W,Y,T,L,M,Q,S={
-aiP:function(d){var x="Basketball stats",w="Email and/or password incorrect",v="Error in the form",u="I know I am getting better at golf because I am hitting fewer spectators.",t="I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",s="Lies, Damn Lies and Statistics",r="Must not be empty"
-return P.as(["About",Z.cd("About"),x,Z.cd(x),"Don't Panic",Z.cd("Don't Panic"),"Douglas Adams",Z.cd("Douglas Adams"),"EDIT",Z.cd("EDIT"),"END",Z.cd("END"),w,Z.cd(w),v,Z.cd(v),"GAMES",Z.cd("GAMES"),"Gerald B Ford",Z.cd("Gerald B Ford"),u,Z.cd(u),t,Z.cd(t),"Internal Error",Z.cd("Internal Error"),s,Z.cd(s),"Loading...",Z.cd("Loading..."),"Login Cancelled",Z.cd("Login Cancelled"),"Mark Twain",Z.cd("Mark Twain"),"Michael Jordan",Z.cd("Michael Jordan"),r,Z.cd(r),"Name",Z.cd("Name"),"Players",Z.cd("Players"),"SAVE",Z.cd("SAVE"),"START",Z.cd("START"),"Save Failed",Z.cd("Save Failed"),"Settings",Z.cd("Settings"),"unknown",Z.cd("unknown")],y.g,y.a)},
-HK:function HK(d){this.a=d}},A,E,U,O,R,K,Z={
-cd:function(d){return new Z.Zq(d)},
-Zq:function Zq(d){this.a=d}},D,B,G,X,F,V,N
+ajc:function(d){var x="Basketball stats",w="Email and/or password incorrect",v="Error in the form",u="I know I am getting better at golf because I am hitting fewer spectators.",t="I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",s="Lies, Damn Lies and Statistics",r="Must not be empty"
+return P.ar(["About",Z.cd("About"),x,Z.cd(x),"Don't Panic",Z.cd("Don't Panic"),"Douglas Adams",Z.cd("Douglas Adams"),"EDIT",Z.cd("EDIT"),"END",Z.cd("END"),w,Z.cd(w),v,Z.cd(v),"GAMES",Z.cd("GAMES"),"Gerald B Ford",Z.cd("Gerald B Ford"),u,Z.cd(u),t,Z.cd(t),"Internal Error",Z.cd("Internal Error"),s,Z.cd(s),"Loading...",Z.cd("Loading..."),"Login Cancelled",Z.cd("Login Cancelled"),"Mark Twain",Z.cd("Mark Twain"),"Michael Jordan",Z.cd("Michael Jordan"),r,Z.cd(r),"Name",Z.cd("Name"),"Players",Z.cd("Players"),"SAVE",Z.cd("SAVE"),"START",Z.cd("START"),"Save Failed",Z.cd("Save Failed"),"Settings",Z.cd("Settings"),"unknown",Z.cd("unknown")],y.g,y.a)},
+HR:function HR(d){this.a=d}},A,E,U,O,R,K,Z={
+cd:function(d){return new Z.ZB(d)},
+ZB:function ZB(d){this.a=d}},D,B,G,X,F,V,N
 a.setFunctionNamesIfNecessary([S,Z])
 C=c[0]
 H=c[1]
@@ -31,19 +31,19 @@ X=c[21]
 F=c[22]
 V=c[23]
 N=c[24]
-S.HK.prototype={
-ga1F:function(){return"messages"}}
+S.HR.prototype={
+ga1R:function(){return"messages"}}
 var z=a.updateTypes(["@(@)"])
-Z.Zq.prototype={
+Z.ZB.prototype={
 $0:function(){return this.a},
 $C:"$0",
 $R:0,
-$S:39};(function installTearOffs(){var x=a._static_1
-x(S,"axU","aiP",0)})();(function inheritance(){var x=a.inherit
-x(S.HK,Z.nr)
-x(Z.Zq,H.ju)})()
-H.akO(b.typeUniverse,JSON.parse('{"apL":"a6","aqU":"a6","apK":"aj","arj":"aj","aur":"hN","apZ":"al","asT":"ac","aqI":"ac","arn":"iv","atx":"e7","apW":"lo","aqp":"j_","aqA":"i6","aqc":"fC","at9":"fC","aro":"l6","aqq":"bK","aqr":"e6","HK":{"nr":[]}}'))
-var y={a:H.am("eV"),g:H.am("h")};(function lazyInitializers(){var x=a.lazy
-x($,"aB8","aoI",function(){return new S.HK(S.aiP(S.axU()))})})()}
-$__dart_deferred_initializers__["CpgqJ5/VW4v0a03euKid9IotHaM="] = $__dart_deferred_initializers__.current
+$S:41};(function installTearOffs(){var x=a._static_1
+x(S,"ays","ajc",0)})();(function inheritance(){var x=a.inherit
+x(S.HR,Z.nq)
+x(Z.ZB,H.jw)})()
+H.alc(b.typeUniverse,JSON.parse('{"aqd":"a6","arm":"a6","aqc":"aj","arO":"aj","auX":"hQ","aqr":"al","atr":"ab","ara":"ab","arS":"ix","au5":"e9","aqo":"lp","aqS":"j0","ar2":"i8","aqF":"fF","atI":"fF","arT":"l8","aqT":"bK","aqU":"e8","HR":{"nq":[]}}'))
+var y={a:H.ai("eY"),g:H.ai("h")};(function lazyInitializers(){var x=a.lazy
+x($,"aBS","apb",function(){return new S.HR(S.ajc(S.ays()))})})()}
+$__dart_deferred_initializers__["Vybg8nUjfj5bLxkg8zfvXQCdYmQ="] = $__dart_deferred_initializers__.current
 //# sourceMappingURL=main.dart.js_1.part.js.map
